6f36311f8cffc35bc62b2fc1d51273230b003369
Omri Avraham <omritk@gmail.com>
2021-07-09 15:45:57 -0600

    Update open_winrm.ps1

--- open_winrm.ps1
+++ open_winrm.ps1
@@ -1,5 +1,6 @@
 $global:logfile_location = "C:\Qualiopenwinrm.txt"
 Start-Transcript -path $global:logfile_location -append
+netsh advfirewall firewall add rule name="Open WinRM Quali" dir=in action=allow protocol=TCP localport=5985
 $ErrorActionPreference = "Stop"
 winrm quickconfig -quiet
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
