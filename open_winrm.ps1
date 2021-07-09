7b0841c665bbf0225a79b0c093955a258dec9609
Omri Avraham <omritk@gmail.com>
2021-07-09 14:52:27 -0600

    Update open_winrm.ps1

--- open_winrm.ps1
+++ open_winrm.ps1
@@ -1,9 +1,8 @@
 $global:logfile_location = "C:\Qualiopenwinrm.txt"
 Start-Transcript -path $global:logfile_location -append
 $ErrorActionPreference = "Stop"
-$result = winrm quickconfig -quiet
-write-host $result
+winrm quickconfig -quiet
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
-$result = winrm get winrm/config
-write-host $result
+winrm get winrm/config
+write-host
